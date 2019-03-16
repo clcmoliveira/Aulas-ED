3c89c9269559df8a03a24745350227f892a94bb5
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2019-03-16 13:41:08 -0300

    Aula 16/03 - Laços

---
+++ lacos.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*
+Esqueleto de um laço Bubblesort
+for(i = 1; i<N; i++){
+	for(j=0; j<n-1; j++){
+		if(v[j] > v[j+1]){
+			aux = v[j];
+			v[j] = v[j]+1;
+			v[j] = aux;
+		}
+	}	
+}
+
+Considerar um vetor de tamanho n
+Comparação: (n-1) + (n-2) + (n-3) + ... + 3 + 2 + 1
+
+1, 2, 3, ..., (n-2), (n-1) é uma PA
+a1 = 1, r = 1, an = n-1, n = n-1
+
+Spa = (a1 + an)*n/2 => Spa = n(n-1)/2 = n²-n/2
+f(x) = x²
+f'(x) = 2x
+*/
+int main(){
+	int *v, n;
+	printf("Digite o tamanho do vetor: \n");
+	scanf("%d", &n);
+	v = (int *) malloc (n * sizeof(int));
+	if(!v)
+		printf("Um erro aconteceu \n");
+
+	return 0;
+}
