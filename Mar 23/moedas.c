c25a447ebeadd4d9dc8e8f2e24a03525337fe4cf
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2019-03-23 15:34:54 -0300

    Aula 23/03
    Atualização do Lancamento de Moedas
    
    Co-Authored-By: Gustavo Oliveira da Silva  <gugra@users.noreply.github.com>

--- Mar 23/moedas.c
+++ Mar 23/moedas.c
@@ -3,15 +3,16 @@
 #include<time.h>
 
 int main() {
-	int n, *v, i, cont1 = 0;
+	int n, i, cont1 = 0;
 	float media;
 	srand(time(0));
 	printf("Numero de Lancamento de Moedas: ");
 	scanf("%d", &n);
-	v = (int *) malloc (n * sizeof(int));
+	//int v = (int *) malloc (n * sizeof(int));
 	for (i = 0; i < n; i++) {
-		v[i] = rand() % 2;
-		cont1 += v[i];
+		//v[i] = rand() % 2;
+		//cont1 += v[i];
+		cont1 += rand() % 2;
 	}
 	printf("\nResultado:\nCaras: %d\nCoroas: %d\n", cont1, n-cont1);
 	return 0;
