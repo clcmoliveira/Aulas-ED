bd54fe3b0fe9f8b1b156beda4cc8d8a996e62ff6
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2020-02-15 15:17:36 -0300

    Update retorno_vetores.c

--- Aula 02/retorno_vetores.c
+++ Aula 02/retorno_vetores.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int * cria_vetor(int t){
-  int v[t], i;
-  for (i = 0; i < t; i++){
-    v[i] = 2 * i;
-   }
-   return v;
+int * cria_vetor_certo(int t){
+  int *v, i;
+  v = (int *) malloc(t * sizeof(int));
+  for(i = 0; i < t; i++){
+    v[i] = 2*i;
+  }
+  return v;
 }
 
 int main(){
-  int * x = cria_vetor(10);
+  int * x = cria_vetor_certo(10);
   int i;
   for (i = 0; i < 10; i++){
     printf("%d\n", x[i]);
