1be559c1cf97f495beccf2b13e53c8aa3e257180
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2020-02-29 14:41:16 -0300

    Update TesteAluno.java

--- Aula 03/TesteAluno.java
+++ Aula 03/TesteAluno.java
@@ -4,18 +4,21 @@ public class TesteAluno {
   public static void main(String[] args) {
     //Declarar a variavel Aluno
     Aluno aluno1;
-    Aluno aluno2;
     
     //Instanciar a variavel
     aluno1 = new Aluno();
-    aluno2 = new Aluno();
     
     //setAnoNasc - Chamar método de encapsulamento
     aluno1.setAnoNasc(2000);
-    aluno2.setAnoNasc(1999);
+    aluno1.setNome("Ana");
+    aluno1.setSexo('F');
+    aluno1.setFormando(true);
     
     
+    //Impressão dos valores na tela
     System.out.println("Ano de nascimento: " + aluno1.getAnoNasc());
-    System.out.println("Ano de nascimento: " + aluno2.getAnoNasc());
+    System.out.println("Nome: " + aluno1.getNome());
+    System.out.println("Sexo: " + aluno1.getSexo());
+    System.out.println("Formando: " + aluno1.getFormando());
   }
 }
