67413992149661879e68276a4b9dfa014c9c7c71
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2020-02-29 13:22:04 -0300

    Create TesteAluno.java

---
+++ Aula 03/TesteAluno.java
@@ -0,0 +1,16 @@
+public class TesteAluno {
+  //Classe de Aplicacao
+  
+  public static void main(String[] args) {
+    //Declarar a variavel Aluno
+    Aluno aluno1;
+    Aluno aluno2;
+    
+    //Instanciar a variavel
+    aluno1 = new Aluno();
+    aluno2 = new Aluno();
+    
+    aluno1.anoNasc = 2000;
+    aluno2.anoNasc = 1999;
+  }
+}
