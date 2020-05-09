16f41e9dc9d1aa0c57ea2832bc34724fa324529c
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2020-05-09 14:04:41 -0300

    Update Modularizacao1.java

--- Aula 04/Modularizacao1.java
+++ Aula 04/Modularizacao1.java
@@ -1,7 +1,7 @@
 public class Modularizacao1{
-  public static void main(String[] args){
-    for(int i=0; i<args.lenght; i++){
-      System.out.ptintln(args[i]);
+    public static void main(String[] args){
+        for(int i=0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
     }
-  }
 }
