d312d31da3c3642b7d9b9dc74a7981ab627c8880
Camila <camila.leite.oliveira@gmail.com>
2020-05-16 15:32:39 -0300

    Internal and External input values

--- Aula 05/TesteLivrodeNotas.java
+++ Aula 05/TesteLivrodeNotas.java
@@ -3,8 +3,14 @@ import javax.swing.JOptionPane;
 public class TesteLivroDeNotas{
     public static void main(String[] args) {
         LivroDeNotas livroDeNotas = new LivroDeNotas();
+        
+        // pedindo ao usuário informar o nome do curso
         String nomeDoCurso = showInputDialog("Digite o nome do Curso");
         livroDeNotas.setNomeDoCurso(nomeDoCurso);
         System.out.println(livroDeNotas.getNomeDoCurso());
+        
+        //informar um nome de curso já criado internamente
+        LivroDeNotas livroDeNotas1 = new LivroDeNotas("ED");
+        System.out.println(livroDeNotas1.getNomeDoCurso());
     }
 }
