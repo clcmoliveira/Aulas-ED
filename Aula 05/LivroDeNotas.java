cff1d8576db9eb2a7cbc07a89636917a8b097f94
Camila <camila.leite.oliveira@gmail.com>
2020-05-16 15:35:02 -0300

    Update LivroDeNotas.java

--- Aula 05/LivroDeNotas.java
+++ Aula 05/LivroDeNotas.java
@@ -1,12 +1,13 @@
 //O livro de notas deve armazenar o nome de um curso e uma lista de alunos que frequentam o curso
 public class LivroDeNotas {
-
+	
+    private static int contador;
     private String nomeDoCurso;
 
     // utilizando o construtor padrão
     public LivroDeNotas(){}
 
-	// utilizando o construtor com o método modificador
+    // utilizando o construtor com o método modificador
     public LivroDeNotas(String nomeDoCurso){
         this.nomeDoCurso = nomeDoCurso;
     }
