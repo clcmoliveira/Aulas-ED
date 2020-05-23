99da8357cc15219cb6623fb7721ca199d30c24f2
Camila <camila.leite.oliveira@gmail.com>
2020-05-23 13:48:29 -0300

    Ordenacao

---
+++ Aula 06/Ordenacao.java
@@ -0,0 +1,6 @@
+public class Ordenacao{
+	private int[] v;
+	public Ordenacao(int [] t){
+		v = new int[t];
+	}
+}
---
+++ Aula 06/TesteOrdenacao.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class TesteOrdenacao{
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner();
+		
+	}
+}
