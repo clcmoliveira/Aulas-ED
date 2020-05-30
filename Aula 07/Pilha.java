5dd5c1b0690f76e622aae33bd747af13c93ffeba
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2020-05-30 13:37:39 -0300

    Update Pilha.java

--- Aula 07/Pilha.java
+++ Aula 07/Pilha.java
@@ -1,8 +1,14 @@
-public class Pilha{
-  private int topo;
-	private int[] dados;
-	public Pilha(int capacidade){
-		dados = new int[capacidade];
-		topo = 0;
-	}
+public class Pilha {
+
+    private int topo;
+    private int[] dados;
+    public Pilha(int capacidade){
+        dados = new int[capacidade];
+        topo = 0;
+    }
+
+    public Pilha(){
+        dados = new int[10];
+        topo = 0;
+    }
 }
