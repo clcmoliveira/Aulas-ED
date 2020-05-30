d8f6249639225c7ad50f24f52fd4455540be3eb0
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2020-05-30 14:24:58 -0300

    Update TestePilha.java

--- Aula 07/TestePilha.java
+++ Aula 07/TestePilha.java
@@ -1,12 +1,12 @@
-public class Testeilha {
+public class TestePilha {
     public static void main(String[] args) {
         Pilha p = new Pilha();
         int i = 1;
-        
+
         while(!p.pilhaCheia()){
             p.push(i++);
         }
-        
+
         while (!p.pilhaVazia()){
             System.out.println(p.pop() + " saiu");
         }
