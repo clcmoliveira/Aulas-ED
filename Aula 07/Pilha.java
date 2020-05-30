4a03d936b9e4a0a8ba2b9931ba650ccbfc94a0a3
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2020-05-30 13:47:52 -0300

    Create pilhaVazia()

--- Aula 07/Pilha.java
+++ Aula 07/Pilha.java
@@ -1,7 +1,8 @@
 public class Pilha {
 
-    private int topo;
-    private int[] dados;
+    private final int topo;
+    private final int[] dados;
+    
     public Pilha(int capacidade){
         dados = new int[capacidade];
         topo = 0;
@@ -10,5 +11,9 @@ public class Pilha {
     public Pilha(){
         dados = new int[10];
         topo = 0;
+    }
+
+    public boolean pilhaVazia(){
+        return topo == 0;
     }
 }
