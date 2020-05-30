56ec4959123a789b0e85e5d1dc752af8d366286d
Camila L. Oliveira <camila.leite.oliveira@gmail.com>
2020-05-30 14:02:23 -0300

    Create TesteRetornoBoolean.java

---
+++ Aula 07/TesteRetornoBoolean.java
@@ -0,0 +1,7 @@
+public class TesteReturnBoolean {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("teste x=10:" + (x==10));
+        System.out.println("teste x=10:" + (x!=10));
+    }
+}
